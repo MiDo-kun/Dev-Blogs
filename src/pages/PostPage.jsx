c0eb47c0b7f6e0ffc287b0f7427633f662077993
MiDo-kun <smidorima781@gmail.com>
2023-05-17 04:58:57 +0800

    Testing next btn

--- src/pages/PostPage.jsx
+++ src/pages/PostPage.jsx
@@ -50,7 +50,7 @@ export default function PostPage() {
       </div>
       {userInfo.id === postInfo.author._id && (
         <div className="flex justify-center text-white">
-          <Link className="" to={`/edit/${postInfo._id}`}>
+          <Link to={`/edit/${postInfo._id}`}>
             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
               <path strokeLinecap="round" strokeLinejoin="round" d="M16.862 4.487l1.687-1.688a1.875 1.875 0 112.652 2.652L10.582 16.07a4.5 4.5 0 01-1.897 1.13L6 18l.8-2.685a4.5 4.5 0 011.13-1.897l8.932-8.931zm0 0L19.5 7.125M18 14v4.75A2.25 2.25 0 0115.75 21H5.25A2.25 2.25 0 013 18.75V8.25A2.25 2.25 0 015.25 6H10" />
             </svg>
@@ -65,9 +65,9 @@ export default function PostPage() {
           Back
         </button>
         {nextPost && (
-          <a href={`/post/${nextPost._id}`} className="flex items-center text-blue-400 hover:underline" onClick={() => window.location.href = `/post/${nextPost._id}`}>
-            <span>Next</span>
-          </a>
+          <Link to={`/post/${postInfo._id}`}>
+            <span className="flex items-center text-blue-400 hover:underline" >Next</span>
+          </Link>
         )}
       </div>
     </div>
