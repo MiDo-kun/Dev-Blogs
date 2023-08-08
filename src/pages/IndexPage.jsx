33004f465f64bdc92cee64ccd33f8b514b8385d7
MiDo-kun <smidorima781@gmail.com>
2023-08-08 23:23:04 +0800

    Adding error handling

--- src/pages/IndexPage.jsx
+++ src/pages/IndexPage.jsx
@@ -20,7 +20,10 @@ const IndexPage = () => {
       response.json().then(posts => {
         setPosts(posts);
       });
-    }).catch(() => alert("Connection Error"));
+    }).catch(() => {
+      alert("Connection Error")
+      location.reload();
+    });
   }, []);
 
   // Calculate the index of the first and last post to display on the current page
@@ -47,13 +50,14 @@ const IndexPage = () => {
       {currentPosts.length > 0 ? (
         currentPosts.map(post => (
           <Post key={post._id} {...post} readingTime={getReadingTime(post.content)} />
-        ))) : <div className="flex flex-wrap content-center  mt-5">
-        <svg className="inine-block animate-spin -ml-1 mr-1 h-5 w-5 text-white" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
-          <circle className="opacity-25" cx="12" cy="12" r="10" stroke="currentColor" strokeWidth="4"></circle>
-          <path className="opacity-75" fill="currentColor" d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"></path>
-        </svg>
-        <p className="text-sm text-gray-200">Fetching Blogs...</p>
-      </div>}
+        ))) :
+        <div className="flex flex-wrap justify-center mt-5">
+          <svg className="inine-block animate-spin -ml-1 mr-1 h-5 w-5 text-white" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
+            <circle className="opacity-25" cx="12" cy="12" r="10" stroke="currentColor" strokeWidth="4"></circle>
+            <path className="opacity-75" fill="currentColor" d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"></path>
+          </svg>
+          <p className="text-sm text-gray-200">Fetching All Blogs...</p>
+        </div>}
       {totalPages > 1 && (
         <div className="flex justify-end gap-2 mt-2">
           <button
--- src/pages/PostPage.jsx
+++ src/pages/PostPage.jsx
@@ -16,12 +16,12 @@ const PostPage = () => {
 
   useEffect(() => {
     const { token } = cookie;
-    fetch(BLOG_ENDPOINT + '/auth/post', {
+    fetch(BLOG_ENDPOINT + '/auth/profile', {
       method: 'GET',
       headers: {
         Authorization: `Bearer ${token}`
       },
-    }).then(response => response.ok && isAuthenticated(true));
+    }).then(response => response.ok && isAuthenticated(true))
 
     fetch(BLOG_ENDPOINT + `/posts/${id}`)
       .then(response => {
@@ -31,8 +31,12 @@ const PostPage = () => {
           postInfo.createdAt = new Date(datePosted).toLocaleDateString('en-US', { month: 'long', day: 'numeric', year: 'numeric' });
 
           setDatePosted(postInfo.createdAt);
-        });
-      });
+        })
+      })
+      .catch(() => {
+        alert("Connection Error!");
+        window.location = window.location.origin;
+      })
 
     fetch(BLOG_ENDPOINT + '/posts')
       .then(response => {
@@ -41,27 +45,28 @@ const PostPage = () => {
           const nextPostID = currentPostIndex < (posts.length - 1) ? posts[currentPostIndex + 1] : null;
           setNextPost(nextPostID);
         });
-      });
+      })
   }, []);
 
   const handleBack = () => {
     navigate('/');
   };
 
-  if (!authenticated) return (
-    <div className="flex flex-wrap content-center  mt-5">
-      <svg className="inine-block animate-spin -ml-1 mr-1 h-5 w-5 text-white" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
-        <circle className="opacity-25" cx="12" cy="12" r="10" stroke="currentColor" strokeWidth="4"></circle>
-        <path className="opacity-75" fill="currentColor" d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"></path>
-      </svg>
-      <p className="text-sm text-gray-200">Fetching Blog...</p>
-    </div>
-  )
+  if (!postInfo)
+    return (
+      <div className="flex flex-wrap justify-center  mt-5">
+        <svg className="inine-block animate-spin -ml-1 mr-1 h-5 w-5 text-white" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
+          <circle className="opacity-25" cx="12" cy="12" r="10" stroke="currentColor" strokeWidth="4"></circle>
+          <path className="opacity-75" fill="currentColor" d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"></path>
+        </svg>
+        <p className="text-sm text-gray-200">Fetching Content...</p>
+      </div>
+    )
 
   return (
     <div className="mx-auto w-[445px] mt-10">
       {authenticated &&
-        <div className="flex justify-center gap-3 text-center text-white text-md">
+        <div className="flex justify-center gap-3 mb-3 text-center text-white text-md">
           <Link to={`/edit/${id}`}>
             <span className="text-blue-500">Edit</span>
           </Link>
